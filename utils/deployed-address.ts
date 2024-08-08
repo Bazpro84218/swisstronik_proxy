33df63cc11d40199f6f02e8a23a76d94723d235a
Bazpro Shaikh <shaikhbazil4@gmail.com>
2024-08-08 10:13:46 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xd508B7De8a96656Bff47F66231D635323729C44B'
+export const ProxyAdmin = '0xD8298e52e28Db44Cb312CE16bb2218723dc2a0d6'
+export const SWTRImplementation = '0xd7C53d1245A30bdA7D29234b8Bfb675Cee593874'
     
